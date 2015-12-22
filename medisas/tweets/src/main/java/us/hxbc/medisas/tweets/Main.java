e5a0729e28d89719657d30268c68104eba923888
Ka-Hing Cheung <khc@hxbc.us>
2015-12-22 14:04:53 -0800

    sample code running

--- medisas/tweets/src/main/java/us/hxbc/medisas/tweets/Main.java
+++ medisas/tweets/src/main/java/us/hxbc/medisas/tweets/Main.java
@@ -1,7 +1,38 @@
 package us.hxbc.medisas.tweets;
 
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
 public class Main {
     public static void main(String[] args) {
+        StatusListener listener = new StatusListener(){
+            public void onStatus(Status status) {
+                System.out.println(status.getUser().getName() + " : " + status.getText());
+            }
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
 
+            @Override
+            public void onScrubGeo(long l, long l1) {
+
+            }
+
+            @Override
+            public void onStallWarning(StallWarning stallWarning) {
+
+            }
+
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.addListener(listener);
+        // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
+        twitterStream.sample();
     }
 }
